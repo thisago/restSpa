1b590a76d6429f779ba299c5d366f09e2876a01f
thisago <thiago@oxyoy.com>
2022-07-19 18:33:35 -0400

    Now the `defaultRoutes` is a const

--- src/restSpa/routes.nim
+++ src/restSpa/routes.nim
@@ -12,20 +12,21 @@ type
     path: string
     routes: seq[UrlPattern]
 
-const routesDefinition*: seq[Route] = @[
-  ("", @[
-    pattern("/", r_home, HttpGet, "home"),
-  ]),
-  ("api", @[
-    pattern("/signin", r_signIn, HttpPost, "signin"),
-    pattern("/signup", r_signUp, HttpPost, "signup"),
-  ]),
-  ("api/admin", @[
-    pattern("/getUser", r_getUser, HttpPost, "getUser"),
-    pattern("/editUser", r_editUser, HttpPost, "editUser"),
-  ]),
-]
+const
+  routesDefinition*: seq[Route] = @[
+    ("", @[
+      pattern("/", r_home, HttpGet, "home"),
+    ]),
+    ("api", @[
+      pattern("/signin", r_signIn, HttpPost, "signin"),
+      pattern("/signup", r_signUp, HttpPost, "signup"),
+    ]),
+    ("api/admin", @[
+      pattern("/getUser", r_getUser, HttpPost, "getUser"),
+      pattern("/editUser", r_editUser, HttpPost, "editUser"),
+    ]),
+  ]
 
-let defaultRoutes* = @[
-  (Http404, r_404)
-]
+  defaultRoutes* = @[
+    (Http404, r_404)
+  ]
